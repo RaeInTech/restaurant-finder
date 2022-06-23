1875cdc6f67e899c4cbed50ba16495ed1770bbc5
rad-a <11688089+rad-a@users.noreply.github.com>
2022-06-23 02:58:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+const { response } = require("express");
+const { request } = require("http");
+require("dotenv").config();
+const PORT = 8000;
+
+let db,
+  dbConnectionStr = process.env.DB_STR,
+  dbName = "sample_mflix",
+  collection;
+
+MongoClient.connect(dbConnectionStr).then((client) => {
+  console.log(`Connected to database`);
+  db = client.db(dbName);
+  collection = db.collection("movies");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/search", async (request, response) => {
+  try {
+    let result = await collection
+      .aggregate([
+        {
+          $search: {
+            autocomplete: {
+              query: `${request.query.query}`,
+              path: "title",
+              fuzzy: {
+                maxEdits: 2,
+                prefixLength: 3,
+              },
+            },
+          },
+        },
+      ])
+      .toArray();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send({ message: error.message });
+    console.log(error);
+  }
+});
+
+app.get("/:id", async (request, response) => {
+  try {
+    let result = await collection.findOne({
+      _id: ObjectId(request.params.id),
+    });
+    response.send(result);
+  } catch {
+    response.status(500).send({ message: error.message });
+  }
+});
